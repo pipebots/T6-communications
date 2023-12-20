0ca3a382917e18f18c833469bfcf64c4e7898548
Hina Nasir <127441792+hinanasir@users.noreply.github.com>
2023-12-20 11:06:27 +0000

    Add files via upload

---
+++ mainSender.py
@@ -0,0 +1,35 @@
+#pycom lopy4 code to be executed on the sender attached to the Rasberrypi4 
+import machine
+from machine import UART
+from network import LoRa
+import socket
+import time
+
+# Configure the UART
+uart = machine.UART(0, baudrate=9600)
+
+# Configure LoRa
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+
+while True:
+    # Read data from serial
+    serial_data = uart.readline()
+
+    if serial_data:
+        # Parse the received data 
+        data_parts = serial_data.decode('utf-8').strip().split(',')
+        
+        if len(data_parts) >= 4:
+            timestamp, cpu_temp, battery_status, blockage_status = data_parts
+            
+            # Print received data 
+            print("Received Data: Timestamp={}, CPU Temp={}, Battery Status={}, Blockage Status={}".format(
+                timestamp, cpu_temp, battery_status, blockage_status))
+
+            # Send data over LoRa
+            lora_data = "{},{},{},{}".format(timestamp, cpu_temp, battery_status, blockage_status)
+            s.send(lora_data)
+
+    time.sleep(1)  # Adjust sleep duration as needed
